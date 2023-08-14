addee762e6a0fc9930ecee9676c145a5cb5edd17
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-14 16:48:24 +0200

    wip try-bind
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- mkosi/run.py
+++ mkosi/run.py
@@ -348,7 +348,7 @@ def finalize_passwd_mounts(root: Path) -> list[PathString]:
         if p.exists():
             options += ["--bind", p, f"/etc/{f}"]
         else:
-            options += ["--bind", "/dev/null", f"/etc/{f}"]
+            options += ["--try-bind", "/dev/null", f"/etc/{f}"]
 
     return options
 
